2e3732db70fe5f4e3465fc6dac953724f107125e
Kyle Smolinsky <kyle.smolinsky@gmail.com>
2021-07-13 06:29:54 -0600

    week 4 assignment

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
 const uploadRouter = require('./routes/uploadRouter');
+const FavoriteRouter = require('./routes/favoriteRouter');
 
 const mongoose = require('mongoose');
 
@@ -61,6 +62,7 @@ app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
 app.use('/imageUpload', uploadRouter);
+app.use('/favorites', FavoriteRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '1185132495336527',
+        clientSecret: 'd719495a67da0a50cc15987f48f78f6b'
+    }
 }
